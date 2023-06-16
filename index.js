c6ab3d32724062a8094bbb5b2a19f1ff20aeec76
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-06-16 08:00:26 -0500

    completed todo list

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
 const express = require('express')
+const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const PORT = 3001 
+const Todo = require('./models/todo')
+
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+mongoose
+      .connect('mongodb://127.0.0.1:27017/todoDB')
+      .then(() => {
+      console.log('Successfully connected to MongoDB.')
+      })
+      .catch((e) => {
+      console.error('Connection error', e.message)
+      })
+
+app.get('/', (req, res) => {
+      Todo.find()
+      .then( result => {
+            res.render('index', { data: result })
+            console.log(result)
+      })
+})
+
+app.post('/', (req, res) => {
+      const todo = new Todo({
+            todo: req.body.todoValue
+      })
+      todo.save()
+      .then(result => {
+            res.redirect('/')
+      })
+})
+
+app.delete('/:id', (req, res) => {
+      Todo.findByIdAndDelete(req.params.id)
+      .then(result => {
+            console.log(result)
+      })
+})
+
+
+app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`)
+})
